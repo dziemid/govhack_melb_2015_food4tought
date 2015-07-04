71410826b5855f4f74c3cfa437b180bef5d543de
rishsharma1996 <rishsharma13@gmail.com>
2015-07-04 18:55:06 +1000

    fix file error

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+import os
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+
+app = Flask(__name__, template_folder=tmpl_dir)
 
 @app.route('/')
 
 def home():
-	return "hello world"
+	return render_template('./index.html')
 
 
 if __name__ == '__main__':
