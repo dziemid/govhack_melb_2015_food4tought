b8515f0556329639ede458624e8fc44ab8a26320
aishsingh <carnage.india95@gmail.com>
2015-07-05 12:01:38 +1000

    more maps

--- app.py
+++ app.py
@@ -6,23 +6,24 @@ tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
 
 @app.route('/')
+@app.route('/index.html')
 
 def home():
 	return render_template('./index.html')
 
 @app.route('/map.html')
 
-def home2():
+def map():
 	return render_template('./map.html')
 
 @app.route('/water.html')
 
-def home2():
+def watermap():
 	return render_template('./water.html')
 
 @app.route('/bio.html')
 
-def home2():
+def biomap():
 	return render_template('./bio.html')
 
 
